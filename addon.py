6aeda328fb57522cf6445d3a37e05ef8a4e687c0
Damon Toumbourou <dtoumbourou@gmail.com>
2016-01-21 23:23:50 +1100

    working on gui

--- addon.py
+++ addon.py
@@ -1,9 +1,12 @@
 from xbmcswift2 import Plugin, xbmcgui
 from resources.lib import skepticsscraper
+import pyxbmct.addonwindow as pyxbmct
+import os
 
 plugin = Plugin()
 
 PODCAST_URL = 'http://www.theskepticsguide.org/podcast/sgu'
+
 
 @plugin.route('/')
 def main_menu():
@@ -17,27 +20,58 @@ def main_menu():
 
     return items
 
-
-@plugin.route('/latest/')
+@plugin.route('/latest_podcast/')
 def latest_podcast():
 
-    content = skepticsscraper.get_podcast(PODCAST_URL)
+    content = skepticsscraper.get_latest_podcast(PODCAST_URL)
     
     items = []
     for i in content:
         items.append({
             'label': i['label'],
-            'path': plugin.url_for('play_podcasts', url=i['path']),
+            'path': plugin.url_for('play_podcast', url=i['path']),
             'thumbnail': i['thumbnail'],
         })
 
     return items
 
+@plugin.route('/latest_podcast/<url>')
+def play_podcast(url):
+    WINDOW_NAME = 'Skeptics Guide to the Universe'
+    WORD = """What's the Word: """
+    NEWS_ITEMS_NAME = "News Items"
 
-@plugin.route('/latest/<url>')
-def play_podcast(url)
+    item = skepticsscraper.get_podcast_content(url)
+
+    for i in item:
+        sub_title = i['title']
+        what_is_word = i['word']
+             
+    window = PodcastWindow(WINDOW_NAME)
+    window.set_sub_title(sub_title)
+    window.set_podcast_content(WORD + what_is_word)
+    window.doModal()
+    del window
+
+class PodcastWindow(pyxbmct.AddonDialogWindow):
     
+    def __init__(self, title=''):
+        super(PodcastWindow, self).__init__(title)
+        self.setGeometry(900,650, 3, 3)
+        # connect a key action (Backspace) to close window
+        self.connect(pyxbmct.ACTION_NAV_BACK, self.close)
 
+    def set_sub_title(self, sub_title):
+        self.textbox = pyxbmct.TextBox()
+        self.placeControl(self.textbox, 0, 0, 2, 4)
+        self.textbox.setText(sub_title)
+
+    def set_podcast_content(self, what_is_word):
+        self.label = pyxbmct.Label(what_is_word)
+        self.placeControl(self.label, 0.3, 0)
+        
+    def set_news_items(self, NEWS_ITEMS_NAME
+ 
 
 if __name__ == '__main__':
     plugin.run()
--- resources/lib/skepticsscraper.py
+++ resources/lib/skepticsscraper.py
@@ -2,32 +2,45 @@ import requests
 import re
 from bs4 import BeautifulSoup as bs
 
+BASE_URL = 'http://www.theskepticsguide.org'
+
 def get_soup(url):
     page = requests.get(url)
     soup = bs(page.text, 'html.parser')
 
     return soup
 
-def get_podcast(url):
+def get_latest_podcast(url):
     # get html
     soup = get_soup(url)
 
     # parse html for useful links 
     content = soup.find('div', {'class': 'podcasts-detail'})
     content = content.find_all('li')
-   
+    
     output = []
 
     for i in content:
         try:
+            path = i.find_all('a')[2].get('href')
+            path = BASE_URL + path
+            print 'path: '
+            print path
+            
+            if not path:
+                break 
+
             label = i.find('a')
             label = i.get_text()
             label = re.search('Episode(.*)$', label).group(0)
-            path = i.find_all('a')[1].get('href')
+            print 'label: '
+            print label    
             
+            print 'img: '
             img = i.find('img').get('src')
+            print img
 
-        except AttributeError:
+        except IndexError:
             continue
         
         items = {
@@ -39,29 +52,73 @@ def get_podcast(url):
         output.append(items)
 
     return output
-#get_podcast('http://www.theskepticsguide.org/podcast/sgu')
+#get_latest_podcast('http://www.theskepticsguide.org/podcast/sgu')
 
 def get_podcast_content(url):
     soup = get_soup(url)
     content = soup.find('div', {'class': 'podcast-detail'})
 
     title = content.find('h1').get_text()
+    print title
 
     specific_content = soup.find_all('div', {'class': 'podcast-segment'})
 
-    news_items = []
+    output = []
+    word = ''
     for i in specific_content:
         find_word = i.find('h3').get_text()
 
         if """What's the Word""" in find_word:
             word = i.find('span', {'class': 'podcast-item-value'}).get_text()
-        
+            print word 
+        """
         if "News Items" in find_word:
             items = i.find_all('li')
             for k in items:
                 label = k.find('span', {'class': 'podcast-item-label'}).get_text()
                 value = k.find('span', {'class': 'podcast-item-value'}).get_text()
-                print value
                 
+                items = {
+                    'label': label,
+                    'value': value,
+                }
+                news_items.append(items)
 
-get_podcast_content('http://www.theskepticsguide.org/podcast/sgu/549')
+        """
+    items = {
+        'title': title,
+        'word': word,  
+    }
+
+    output.append(items)
+
+    return output
+#get_podcast_content('http://www.theskepticsguide.org/podcast/sgu/549')
+
+
+def get_news_items(url):
+    soup = get_soup(url)
+    content = soup.find_all('div', {'class': 'podcast-segment'})
+    
+    news_items = []
+    for i in content:
+        find_word = i.find('h3').get_text()
+
+        if "News Items" in find_word:
+            items = i.find_all('li')
+            for k in items:
+                news_label = k.find('span', {'class': 'podcast-item-label'}).get_text()
+                news_link = k.find('span', {'class': 'podcast-item-value'}).get_text()
+                print 'label: '
+                print news_label
+                print 'link: '
+                print news_link
+                items = {
+                    'news_label': news_label,
+                    'news_link': news_link,
+                }
+
+                news_items.append(items)
+
+    return news_items
+get_news_items('http://www.theskepticsguide.org/podcast/sgu/549')
